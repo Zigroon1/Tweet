ea781f7e16add964a37fc972225a7901c0b9d778
Zigroon1 <nellikt@bk.ru>
2018-04-10 00:52:27 +0300

    Оформление без серверной части.

---
+++ app/src/main/java/com/vova/tweet/activity/UserInfoActivity.java
@@ -0,0 +1,136 @@
+package com.vova.tweet.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.squareup.picasso.Picasso;
+import com.vova.tweet.R;
+import com.vova.tweet.adapter.TweetAdapter;
+import com.vova.tweet.pojo.Tweet;
+import com.vova.tweet.pojo.User;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class UserInfoActivity extends AppCompatActivity {
+    public static final String USER_ID = "userId";
+
+    private ImageView userImageView;
+    private TextView nameTextView;
+    private TextView nickTextView;
+    private TextView descriptionTextView;
+    private TextView locationTextView;
+    private TextView followingCountTextView;
+    private TextView followersCountTextView;
+    private Toolbar toolbar;
+
+    private RecyclerView tweetsRecyclerView;
+    private TweetAdapter tweetAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+// присвоение id полям
+        userImageView = findViewById(R.id.user_image_view);
+        nameTextView = findViewById(R.id.user_name_text_view);
+        nickTextView = findViewById(R.id.user_nick_text_view);
+        descriptionTextView = findViewById(R.id.user_description_text_view);
+        locationTextView = findViewById(R.id.user_location_text_view);
+        followingCountTextView = findViewById(R.id.following_count_text_view);
+        followersCountTextView = findViewById(R.id.followers_count_text_view);
+        toolbar = findViewById(R.id.toolbar);
+        //подключение тулбара
+        setSupportActionBar(toolbar);
+
+        initRecyclerView();
+
+        loadUserInfo();
+        loadTweets();
+    }
+// подключение меню
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.user_info_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_search) {
+            Intent intent = new Intent(this, SearchUsersActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+
+    private void loadTweets() {
+        Collection<Tweet> tweets = getTweets();
+        tweetAdapter.setItems(tweets);
+    }
+
+    // запосление списка твитов
+    private Collection<Tweet> getTweets() {
+        return Arrays.asList(
+                new Tweet(getUser(), 1L, "Thu Dec 13 07:31:08 +0000 2017", "Очень длинное описание твита 1",
+                        4L, 4L, "https://www.w3schools.com/w3css/img_fjords.jpg"),
+
+                new Tweet(getUser(), 2L, "Thu Dec 12 07:31:08 +0000 2017", "Очень длинное описание твита 2",
+                        5L, 5L, "https://www.w3schools.com/w3images/lights.jpg"),
+
+                new Tweet(getUser(), 3L, "Thu Dec 11 07:31:08 +0000 2017", "Очень длинное описание твита 3",
+                        6L, 6L, "https://www.w3schools.com/css/img_mountains.jpg")
+        );
+    }
+
+    private void initRecyclerView() {
+        tweetsRecyclerView = findViewById(R.id.tweets_recycler_view);
+        tweetsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tweetAdapter = new TweetAdapter();
+        tweetsRecyclerView.setAdapter(tweetAdapter);
+    }
+
+    private void loadUserInfo() {
+        User user = getUser();
+        displayUserInfo(user);
+    }
+
+    // установка текста в textviev
+    private void displayUserInfo(User user) {
+        Picasso.with(this).load(user.getImageUrl()).into(userImageView);
+        nameTextView.setText(user.getName());
+        nickTextView.setText(user.getNick());
+        descriptionTextView.setText(user.getDescription());
+        locationTextView.setText(user.getLocation());
+
+        String followingCount = String.valueOf(user.getFollowingCount());
+        followingCountTextView.setText(followingCount);
+
+        String followersCount = String.valueOf(user.getFollowersCount());
+        followersCountTextView.setText(followersCount);
+// установка заголовка нашего экрана
+        getSupportActionBar().setTitle(user.getName());
+    }
+// возвращение тестового объекта
+    private User getUser() {
+        return new User(
+                1L,
+                "http://i.imgur.com/DvpvklR.png",
+                "DevColibri",
+                "devcolibri",
+                "Sample description",
+                "USA",
+                42,
+                42
+        );
+    }
+}
---
+++ app/src/main/java/com/vova/tweet/adapter/UsersAdapter.java
@@ -0,0 +1,85 @@
+package com.vova.tweet.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.vova.tweet.R;
+import com.vova.tweet.pojo.User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+// создание адаптера для списка екрана
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UserViewHolder> {
+    private List<User> userList = new ArrayList<>();
+    private OnUserClickListener onUserClickListener;
+
+    public UsersAdapter(OnUserClickListener onUserClickListener) {
+        this.onUserClickListener = onUserClickListener;
+    }
+
+    @Override
+    public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.user_item_view, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(UserViewHolder holder, int position) {
+        User user = userList.get(position);
+        holder.bind(user);
+    }
+
+    public void setItems(Collection<User> users) {
+        userList.addAll(users);
+        notifyDataSetChanged();
+    }
+
+    public void clearItems() {
+        userList.clear();
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    class UserViewHolder extends RecyclerView.ViewHolder {
+        private ImageView userImageView;
+        private TextView nameTextView;
+        private TextView nickTextView;
+
+        public UserViewHolder(View itemView) {
+            super(itemView);
+            userImageView = itemView.findViewById(R.id.profile_image_view);
+            nameTextView = itemView.findViewById(R.id.user_name_text_view);
+            nickTextView = itemView.findViewById(R.id.user_nick_text_view);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    User user = userList.get(getLayoutPosition());
+                    onUserClickListener.onUserClick(user);
+                }
+            });
+        }
+
+        public void bind(User user) {
+            nameTextView.setText(user.getName());
+            nickTextView.setText(user.getNick());
+            Picasso.with(itemView.getContext()).load(user.getImageUrl()).into(userImageView);
+        }
+    }
+
+    public interface OnUserClickListener {
+        void onUserClick(User user);
+    }
+}
